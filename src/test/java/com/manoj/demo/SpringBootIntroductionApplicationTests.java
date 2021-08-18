a321bea38c12082ee682d22437e117ce967563f2
Manoj Kumar <manoj.er.kumar@gmail.com>
2021-08-18 17:23:30 +0530

    Spring Boot initial commit

---
+++ src/test/java/com/manoj/demo/SpringBootIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
